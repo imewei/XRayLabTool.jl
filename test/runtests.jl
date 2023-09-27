acfb04f74da64f18cde6e65d60792014a31544ec
Wei Chen <wchen@anl.gov>
2023-09-26 23:11:34 -0500

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,25 +1,40 @@
 using XRayLabTool
 using Test
 
-# Define a helper function to check if two arrays are approximately equal
-function assert_approx_equal(actual, expected; tol = 1e-6)
-    @test length(actual) == length(expected)
-    @test all(abs.(actual - expected) .< tol)
+@testset "Refrac Function Tests" begin
+    data = Refrac(["SiO2", "H2O"], [1, 2, 3.5, 5, 6], [2.2, 1.0])
+
+    @test begin
+        disp_value = data["SiO2"]["Dispersion"][3]
+        @test abs(disp_value - 3.814915113516378e-5) < eps()
+    end
+
+    @test begin
+        f1_value = data["SiO2"]["f1"][1]
+        @test abs(f1_value - 29.448492570232553) < eps()
+    end
+
+    @test begin
+        reSLD_value = data["SiO2"]["reSLD"][5]
+        @test abs(reSLD_value - 1.8979332462750346e-5) < eps()
+    end
 end
 
-@testset "XRayLabTool.jl" begin
-    data = Refrac(["SiO2", "H2O"], [1.0, 2.0, 3.5, 5.0, 6.0], [2.2, 1.0])
-
-    @test haskey(data, "SiO2")
-    @test haskey(data, "H2O")
-
+@testset "SubRefrac Function Tests" begin
     si = SubRefrac("Si", [20.0], 2.33)
 
-    @test_approx_equal data["SiO2"].Dispersion[3], 3.814915113516378e-5
-    @test_approx_equal data["SiO2"].f1[1], 29.448492570232553
-    @test_approx_equal data["SiO2"].reSLD[5], 1.8979332462750346e-5
+    @test begin
+        disp_value = si["Dispersion"][1]
+        @test abs(disp_value - 1.2096870811637957e-6) < eps()
+    end
 
-    @test_approx_equal si.Dispersion[1], 1.2096870811637957e-6
-    @test_approx_equal si.f1[1], 14.048053002299712
-    @test_approx_equal si.reSLD[1], 1.977791074150516e-5
+    @test begin
+        f1_value = si["f1"][1]
+        @test abs(f1_value - 14.048053002299712) < eps()
+    end
+
+    @test begin
+        reSLD_value = si["reSLD"][1]
+        @test abs(reSLD_value - 1.977791074150516e-5) < eps()
+    end
 end
